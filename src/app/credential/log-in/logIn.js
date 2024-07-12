c97905401d84b913b0c17620b09317df50616d69
Ujjwal1375 <ujjwal.keshwarvani@mediadynox.com>
2024-07-12 11:04:01 +0530

    create login page for desktop

--- src/app/credential/log-in/logIn.js
+++ src/app/credential/log-in/logIn.js
@@ -1,6 +1,7 @@
 "use client";
 
 import CredentialHero from "@/components/CredentialHero/CredentialHero";
+import OtherLoginOpts from "@/components/OtherLoginOpts/OtherLoginOpts";
 import style from "@/styles/credentail.module.css";
 import styles from "@/styles/credentail.module.css";
 import Link from "next/link";
@@ -29,26 +30,41 @@ const LogIn = () => {
                     <label>Email Address</label>
                     <input type="emmail" placeholder="Email" required />
                   </li>
+
                   <li>
-                    <label>Password</label>
+                    <div className={style.passwordLabel}>
+                      <label>Password</label>
+                      <button
+                        style={{
+                          outline: "none",
+                          border: "none",
+                          padding: "0",
+                          cursor: "pointer",
+                          background: "transparent",
+                        }}
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className={styles.toggleButton}
+                      >
+                        {showPassword ? (
+                          <img src="/hide.svg" alt="" />
+                        ) : (
+                          <img src="/show.svg" alt="" />
+                        )}
+                      </button>
+                    </div>
                     <input
                       type={showPassword ? "text" : "password"}
                       value={password}
+                      required
                       onChange={(e) => setPassword(e.target.value)}
                       className={styles.passwordInput}
-                      placeholder="Enter your password"
+                      placeholder="password"
                     />
-                    <button
-                      type="button"
-                      onClick={togglePasswordVisibility}
-                      className={styles.toggleButton}
-                    >
-                      {showPassword ? "Hide" : "Show"}
-                    </button>
                   </li>
 
-                  <li>
-                    <button>Sign up</button>
+                  <li className={style.formSubOpts}>
+                    <input type="submit" value="Sign up" />
                     <p>
                       Already have an ccount?{" "}
                       <Link href="/credential/sign-up">Sign up</Link>
@@ -56,6 +72,8 @@ const LogIn = () => {
                   </li>
                 </ul>
               </form>
+
+              <OtherLoginOpts />
             </div>
           </div>
         </div>
---
+++ src/components/OtherLoginOpts/OtherLoginOpts.jsx
@@ -0,0 +1,39 @@
+import style from "../../styles/otherLoginOpts.module.css";
+
+const OtherLoginOpts = () => {
+  return (
+    <>
+      <div className={style.socialLoginOpts}>
+        <label>OR Continue with</label>
+        <ul>
+          <li>
+            <button>
+              <i>
+                <img src="/fb.svg" alt="" />
+              </i>
+              Facebook
+            </button>
+          </li>
+          <li>
+            <button>
+              <i>
+                <img src="/google.svg" alt="" />
+              </i>
+              Google
+            </button>
+          </li>
+          <li>
+            <button>
+              <i>
+                <img src="/apple.svg" alt="" />
+              </i>
+              Apple
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default OtherLoginOpts;
