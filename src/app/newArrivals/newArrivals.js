f548af1cc8e9185120bb5bfa37ec34453bff7e8a
Ujjwal1375 <ujjwal.keshwarvani@mediadynox.com>
2024-07-16 18:33:57 +0530

    new arrival page

--- src/app/newArrivals/newArrivals.js
+++ src/app/newArrivals/newArrivals.js
@@ -3,7 +3,12 @@ import InnerBanner from "@/components/InnerPageBanner/InnerBanner";
 const NewArrivals = () => {
   return (
     <>
-      <InnerBanner />
+      <InnerBanner
+        tag="Limited time only"
+        heading="Get 30% off"
+        bgImg="/newArrivalBg.png"
+        descrip={`Shoes made with your comfort in mind so you can put all of your \n focus into your next session.`}
+      />
     </>
   );
 };
--- src/components/InnerPageBanner/InnerBanner.jsx
+++ src/components/InnerPageBanner/InnerBanner.jsx
@@ -1,18 +1,18 @@
 import style from "../../styles/innerBanner.module.css";
 
-const InnerBanner = () => {
+const InnerBanner = ({ tag, heading, descrip, bgImg }) => {
   return (
     <>
       <div className={style.innerBannerMain}>
         <div className="container">
           <div className={style.InnerBannerItems}>
+            <figure>
+              <img src={bgImg} alt="" />
+            </figure>
             <div className={style.content}>
-              <p className={style.tag}>Shoes</p>
-              <h2>Shoes</h2>
-              <p className={style.descrip}>
-                Shoes made with your comfort in mind so you can put all of your
-                focus into your next session.
-              </p>
+              <p className={style.tag}>{tag}</p>
+              <h2>{heading}</h2>
+              <p className={style.descrip}>{descrip}</p>
             </div>
           </div>
         </div>
