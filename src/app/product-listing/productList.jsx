617ea25d3d2c3c5d6c8bf1f96e69f6ec5e5f1fef
Ujjwal1375 <ujjwal.keshwarvani@mediadynox.com>
2024-07-17 18:49:25 +0530

    solve error

--- src/app/product-listing/productList.jsx
+++ src/app/product-listing/productList.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import PRODUCTS_DATA from "./PRODUCTS_DATA";
+import style from "../../styles/productFilter.module.css";
 
 const ProductList = () => {
   return (
@@ -8,38 +9,36 @@ const ProductList = () => {
       <div className="productListMain">
         <div className="container">
           <div className="productListItems">
-            {PRODUCTS_DATA.map(
-              ({ id, proName, realprice, offerPrice, tag, proImg }) => {
-                return (
-                  <>
-                    <div className="card" key={id}>
-                      <div className="tag"></div>
-                      <div className="proImg">
-                        <figure>
-                          <img src="/shoe-1.svg" alt="" />
-                        </figure>
+            {PRODUCTS_DATA.map(({ id, proName, realprice, offerPrice }) => {
+              return (
+                <>
+                  <div className="card" key={id}>
+                    <div className="tag"></div>
+                    <div className="proImg">
+                      <figure>
+                        <img src="/shoe-1.svg" alt="" />
+                      </figure>
 
-                        <div className={style.proDetail}>
-                          <p className={style.proName}>{proName}</p>
+                      <div className={style.proDetail}>
+                        <p className={style.proName}>{proName}</p>
 
-                          <div className={style.bar}>
-                            <div className={style.price}>
-                              <p className={style.realPrice}>
-                                <del>{realprice}</del>
-                              </p>
-                              <p className={style.offerPrice}>{offerPrice}</p>
-                            </div>
-                            <div className={style.learnMore}>
-                              <img src="learnMore.svg" alt="" />
-                            </div>
+                        <div className={style.bar}>
+                          <div className={style.price}>
+                            <p className={style.realPrice}>
+                              <del>{realprice}</del>
+                            </p>
+                            <p className={style.offerPrice}>{offerPrice}</p>
+                          </div>
+                          <div className={style.learnMore}>
+                            <img src="learnMore.svg" alt="" />
                           </div>
                         </div>
                       </div>
                     </div>
-                  </>
-                );
-              }
-            )}
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
